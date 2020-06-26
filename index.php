fce14a5a35dcfc62a1430639d2b5546c1226e18c
Jason Robinson <jasonrobinson179@gmail.com>
2020-06-26 15:12:07 +0100

    .

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css/style.min.css">
 	<script defer src="js/fretNotes.min.js"></script>
-	<script defer src="js/guitar_scales.js"></script>
+	<script defer src="js/guitar_scales.min.js"></script>
 	<link rel="icon" type="image/png" href="favicon.png">
 	<title>Guitar Scales</title>
 
